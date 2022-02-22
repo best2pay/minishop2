44a656e5fadbf56f13cdc28e2edbf0f5e942fbc5
best2pay <github@best2pay.net>
2022-02-22 13:46:05 +0300

    localization

--- core/components/minishop2/lexicon/en/msp.best2pay.inc.php
+++ core/components/minishop2/lexicon/en/msp.best2pay.inc.php
@@ -2,3 +2,11 @@
 
 $_lang['setting_ms2_payment_best2pay_test_mode'] = 'Test mode';
 $_lang['setting_ms2_payment_best2pay_test_mode_desc'] = 'Mode for testing or production';
+$_lang['setting_ms2_payment_best2pay_wrong_currency'] = 'Wrong currency';
+$_lang['setting_ms2_payment_best2pay_desc'] = 'Order payment';
+$_lang['setting_ms2_payment_best2pay_delivery'] = 'Delivery';
+$_lang['setting_ms2_payment_best2pay_sale'] = 'Sale';
+$_lang['setting_ms2_payment_best2pay_empty_data'] = 'Empty data';
+$_lang['setting_ms2_payment_best2pay_nonvalid_xml'] = 'Non valid XML was received';
+$_lang['setting_ms2_payment_best2pay_invalid_signature'] = 'Invalid signature';
+$_lang['setting_ms2_payment_best2pay_cancel_order'] = 'Order cancelled';
--- core/components/minishop2/lexicon/ru/msp.best2pay.inc.php
+++ core/components/minishop2/lexicon/ru/msp.best2pay.inc.php
@@ -2,3 +2,11 @@
 
 $_lang['setting_ms2_payment_best2pay_test_mode'] = 'Тестовый режим';
 $_lang['setting_ms2_payment_best2pay_test_mode_desc'] = 'Тестовый режим работы';
+$_lang['setting_ms2_payment_best2pay_wrong_currency'] = 'Неверная валюта';
+$_lang['setting_ms2_payment_best2pay_desc'] = 'Оплата заказа';
+$_lang['setting_ms2_payment_best2pay_delivery'] = 'Доставка';
+$_lang['setting_ms2_payment_best2pay_sale'] = 'Скидка';
+$_lang['setting_ms2_payment_best2pay_empty_data'] = 'Пустые данные';
+$_lang['setting_ms2_payment_best2pay_nonvalid_xml'] = 'Получен недействительный XML';
+$_lang['setting_ms2_payment_best2pay_invalid_signature'] = 'Неверная подпись';
+$_lang['setting_ms2_payment_best2pay_cancel_order'] = 'Заказ отменён';
